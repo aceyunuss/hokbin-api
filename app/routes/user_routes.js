dd43317d6740ec56337f6b595e74123f9451bbf9
Ace <aceyunus@gmail.com>
2022-10-27 15:11:36 +0700

    login not using jwt

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-  const user = require("../controllers/user.js");
-  const auth = require("../middleware/auth.js");
+  const user = require("../controllers/user");
+  const auth = require("../middleware/auth");
   var router = require("express").Router();
 
   router.post("/", auth, user.create);
-  router.post("/login", auth, user.login);
-  router.post("/logins", auth, user.logins);
+  router.post("/login", user.login);
+  router.post("/logins", user.logins);
   router.get("/filter", auth, user.findFilter);
   router.get("/", auth, user.findAll);
   router.get("/:id", auth, user.findOne);
